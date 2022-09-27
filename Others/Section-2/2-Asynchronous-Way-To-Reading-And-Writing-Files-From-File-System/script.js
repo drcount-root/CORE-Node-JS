75fcb9a3f0407b342d28759399c4c03ce0ec3069
drcount-root <about.subhamsahu@gmail.com>
2022-09-27 19:38:26 +0530

    27th Sept 2022

--- Others/Section-2/2-Asynchronous-Way-To-Reading-And-Writing-Files-From-File-System/script.js
+++ Others/Section-2/2-Asynchronous-Way-To-Reading-And-Writing-Files-From-File-System/script.js
@@ -32,3 +32,28 @@ fs.writeFile("../FakeFileSystem/output.txt", toWrite, "utf-8", (err) => {
 });
 
 console.log("Writing file ...");
+//
+//
+//
+//
+//
+// Play non-blocking / asynchronous way with callback fnc
+
+fs.readFile("../txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR");
+
+  fs.readFile(`../txt/${data1}.txt`, "utf-8", (err, data2) => {
+    if (err) return console.log("ERROR");
+    console.log(data2);
+    fs.readFile("../txt/append.txt", "utf-8", (err, data3) => {
+      if (err) return console.log("ERROR");
+      console.log(data3);
+
+      fs.writeFile("../txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        if (err) return console.log("ERROR");
+        console.log("File written!");
+      });
+    });
+  });
+});
+console.log("Will read file!");
