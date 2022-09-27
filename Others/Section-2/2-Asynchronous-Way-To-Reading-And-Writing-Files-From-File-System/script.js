bfd49c7de6c7778c037a18d1cec8bbf40770ce13
drcount-root <about.subhamsahu@gmail.com>
2022-09-27 17:59:20 +0530

    27th Sept 2022

--- Others/Section-2/2-Asynchronous-Way-To-Reading-And-Writing-Files-From-File-System/script.js
+++ Others/Section-2/2-Asynchronous-Way-To-Reading-And-Writing-Files-From-File-System/script.js
@@ -2,22 +2,22 @@
 const fs = require("fs");
 //
 // Asynchronous way of reading a file from file system
-// readFile() takes 2 arguments - 1. path to the file
+// readFile() takes 3 arguments - 1. path to the file
 //                                2. character encoding
 //                                3. callback fnc
 //
-// fs.readFile("../FakeFileSystem/input.txt", "utf-8", (err, data) => {
-//   if (err) return console.log("ERROR!");
-//   console.log("\n", data, "\n");
-// });
-// console.log("\nReading file ...");
+fs.readFile("../FakeFileSystem/input.txt", "utf-8", (err, data) => {
+  if (err) return console.log("ERROR!");
+  console.log("\n", data, "\n");
+});
+console.log("\nReading file ...");
 //
 //
 //
 //
 //
 // Asynchronous way of writing inside a file of file system
-// writeFile() takes 2 arguments - 1. path to the file
+// writeFile() takes 4 arguments - 1. path to the file
 //                                 2. what to write
 //                                 3. character encoding
 //                                 4. callback fnc
