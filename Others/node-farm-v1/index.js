db58db27c10617c7f84de0576cdec6f3f177e1a1
drcount-root <about.subhamsahu@gmail.com>
2022-09-29 08:39:11 +0530

    29th Sept 2022

---
+++ Others/node-farm-v1/index.js
@@ -0,0 +1,143 @@
+//////////////////////////////////
+// FILES
+
+// const fs = require("fs");
+
+// Synchronous Reading
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// // Synchronous Writing
+
+// const textOut = `This is what we know about the avocado:\n ${textIn}.`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("\nFile written!\n");
+// console.log(fs.readFileSync("./txt/output.txt", "utf-8"));
+
+// Asynchronous Reading
+
+// fs.readFile("./txt/input.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+// console.log("Reading file ...");
+
+// Asynchronous Writing
+
+// const toWrite = `👽 Yo Yo man 👽`;
+// fs.writeFile("./txt/output.txt", toWrite, (err) => {
+//   if (err) throw err;
+
+//   console.log("File written!");
+//   console.log("\nFile content\n", fs.readFileSync("./txt/output.txt", "utf-8"));
+// });
+
+// console.log("Writing file ...");
+
+// Non-blocking, asynchronous way
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR");
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     if (err) return console.log("ERROR");
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       if (err) return console.log("ERROR");
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         if (err) return console.log("ERROR");
+//         console.log("File written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+//
+
+//////////////////////////////////
+// SERVER
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  }
+
+  // Product page
+  else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    res.writeHead(200, { "Content-type": "text/html" });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+
+  // API
+  else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    console.log(dataObj);
+  }
+
+  // Not found
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+
+  // res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
